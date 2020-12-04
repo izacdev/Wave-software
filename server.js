fd7f13a7f63dc4df56c02d6e44a2e2e67c5e2560
izac dev <izacwhisper3@gmail.com>
2020-12-04 21:59:37 +0300

    new changes

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("my-app"))
+    app.use(express.static("my-app/build"))
 }
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`running on ${PORT}`))
