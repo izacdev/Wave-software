d0580af449e63ea4bccddb7d44fe90ec92084bae
izac dev <izacwhisper3@gmail.com>
2021-03-02 11:10:07 +0300

    newcommit

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("my-app/build"))
+    app.use(express.static(__dirname, "my-app/public"))
+    app.use(express.static(__dirname,  "public/index.html"))
 }
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'my-app/build', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/my-app/public/index.html"));
   });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`running on ${PORT}`))
+const port = process.env.PORT || 4000;
+app.listen(port, console.log(`engine running on ${port}`))
